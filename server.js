acbeccf1f39b37a3208bac0436870c79b13f0993
nimrod aviam <nimrod.a6@gmail.com>
2022-08-01 19:27:58 +0300

    a

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '50mb' }));
 // app.use(express.urlencoded());
 app.use(express.urlencoded({ limit: '50mb' }));
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
