1c48a803966475a3e7d6d5ab62e557b76334b237
nimrod aviam <nimrod.a6@gmail.com>
2022-08-01 18:58:31 +0300

    aa

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ const http = require('http').createServer(app)
 // Express App Config
 app.use(cookieParser())
 // app.use(express.json())
-// app.use(express.json());
-
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded());
 
-// app.use(express.urlencoded({ limit: '50mb' }));
+// app.use(express.urlencoded());
+app.use(express.urlencoded({ limit: '50mb' }));
+
 app.use(express.static('public'))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
