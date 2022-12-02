d6b81a21c4fe3594f28bdc1de1cdc07049f6b07e
nimrod aviam <nimrod.a6@gmail.com>
2022-12-02 10:20:14 +0200

    build

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -7,7 +7,7 @@ async function login(req, res) {
         const user = await authService.login(username, password)
         const loginToken = authService.getLoginToken(user)
         logger.info('User login: ', user)
-        res.cookie('loginToken', loginToken, {sameSite: 'None', secure: true})
+        res.cookie('loginToken', loginToken, { sameSite: 'None', secure: true })
         res.json(user)
     } catch (err) {
         logger.error('Failed to Login ' + err)
@@ -18,14 +18,14 @@ async function login(req, res) {
 async function signup(req, res) {
     try {
         const credentials = req.body
-        // Never log passwords
+        // Never log passwords.
         // logger.debug(credentials)
         const account = await authService.signup(credentials)
         logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
         const user = await authService.login(credentials.username, credentials.password)
         logger.info('User signup:', user)
         const loginToken = authService.getLoginToken(user)
-        res.cookie('loginToken', loginToken, {sameSite: 'None', secure: true})
+        res.cookie('loginToken', loginToken, { sameSite: 'None', secure: true })
         res.json(user)
     } catch (err) {
         logger.error('Failed to signup ' + err)
@@ -33,7 +33,7 @@ async function signup(req, res) {
     }
 }
 
-async function logout(req, res){
+async function logout(req, res) {
     try {
         res.clearCookie('loginToken')
         res.send({ msg: 'Logged out successfully' })
