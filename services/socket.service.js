858ee2f7201d0c733c85bb3e4ce136dcf91d2330
nimrod aviam <nimrod.a6@gmail.com>
2023-01-25 16:48:34 +0200

    add socket.io

--- services/socket.service.js
+++ services/socket.service.js
@@ -13,34 +13,20 @@ function setupSocketAPI(http) {
         socket.on('disconnect', socket => {
             logger.info(`Socket disconnected [id: ${socket.id}]`)
         })
-        socket.on('chat-set-topic', topic => {
-            if (socket.myTopic === topic) return
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-                logger.info(`Socket is leaving topic ${socket.myTopic} [id: ${socket.id}]`)
+        socket.on('set-story-socket', storyId => {
+            logger.info(`New connected story-socket [id: ${storyId}]`)
+            if (socket.myStory === storyId) return
+            if (socket.myStory) {
+                socket.leave(socket.myStory)
+                logger.info(`Socket is leaving topic ${socket.myStory} [id: ${socket.id}]`)
             }
-            socket.join(topic)
-            socket.myTopic = topic
+            socket.join(storyId)
+            socket.myStory = storyId
         })
-        socket.on('chat-send-msg', msg => {
-            logger.info(`New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myTopic}`)
-            // emits to all sockets:
-            // gIo.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            gIo.to(socket.myTopic).emit('chat-add-msg', msg)
-        })
-        socket.on('user-watch', userId => {
-            logger.info(`user-watch from socket [id: ${socket.id}], on user ${userId}`)
-            socket.join('watching:' + userId)
-            
-        })
-        socket.on('set-user-socket', userId => {
-            logger.info(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
-            socket.userId = userId
-        })
-        socket.on('unset-user-socket', () => {
-            logger.info(`Removing socket.userId for socket [id: ${socket.id}]`)
-            delete socket.userId
+        socket.on('this-user-add-comment', story => {
+            logger.info(`New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myStory}`)
+            // gIo.to(socket.myStory).emit('other-user-add-comment', story)
+            gIo.emit('other-user-add-comment', story)
         })
 
     })
@@ -57,7 +43,7 @@ async function emitToUser({ type, data, userId }) {
     if (socket) {
         logger.info(`Emiting event: ${type} to user: ${userId} socket [id: ${socket.id}]`)
         socket.emit(type, data)
-    }else {
+    } else {
         logger.info(`No active socket for user: ${userId}`)
         // _printSockets()
     }
@@ -107,9 +93,9 @@ module.exports = {
     // set up the sockets service and define the API
     setupSocketAPI,
     // emit to everyone / everyone in a specific room (label)
-    emitTo, 
+    emitTo,
     // emit to a specific user (if currently active in system)
-    emitToUser, 
+    emitToUser,
     // Send to all sockets BUT not the current socket - if found
     // (otherwise broadcast to a room / to all)
     broadcast,
